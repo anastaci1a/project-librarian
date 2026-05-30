3cc8cc31bbce4c4b0be5da03f1369a1830abd443
Ana Jahnel <48846277+anastaci1a@users.noreply.github.com>
2026-05-29 21:27:48 -0400

    added utilities

---
+++ src/library/util.py
@@ -0,0 +1,148 @@
+# dep
+
+from __future__ import annotations
+
+from enum   import Enum
+from os     import PathLike
+from typing import Any
+
+import json
+
+
+# const
+
+BOX_CHARS_DEFAULT = "─│╭╮╰╯"
+
+
+# json
+
+class JSONFile:
+    class ImmutEncoder(json.JSONEncoder):
+        def default(self, o):
+            if isinstance(o, set):
+                return list(o)
+            else:
+                return super().default(o)
+
+    @staticmethod
+    def write_dumps(
+            outfile: str | PathLike[str],
+            json_serializable: Any,
+            indent    = 4,
+            sort_keys = False
+    ) -> str:
+        json_str = json.dumps(
+            json_serializable,
+            indent=indent,
+            sort_keys=sort_keys,
+            cls=JSONFile.ImmutEncoder
+        )
+        with open(outfile, "w") as file:
+            file.write(json_str)
+        return json_str
+
+    @staticmethod
+    def loads(infile: str | PathLike[str]) -> Any:
+        with open(infile, "r") as file:
+            json_str = "".join(file.readlines())
+        return json.loads(json_str)
+
+
+# arg
+
+class ArgParse:
+    @staticmethod
+    def str_list(arg: str|list[str]|None) -> list[str]:
+        if isinstance(arg, str): return [arg]
+        return arg
+
+
+# cli
+
+class Ansi(Enum):
+    F_RESET     = "\033[0m"
+    F_BOLD      = "\033[1m"
+    F_ITALIC    = "\033[3m"
+    F_UNDERLINE = "\033[4m"
+
+    def __str__(self):
+        return self.value
+
+class Cli:
+    @staticmethod
+    def print_boxed_text(
+            text:  str,
+            chars: str|None = BOX_CHARS_DEFAULT
+    ) -> str|None:
+        if chars is None:
+            chars = BOX_CHARS_DEFAULT
+
+        if len(chars) != 6: return None # err
+        c_hori, c_vert, c_tl, c_tr, c_bl, c_br = chars
+
+        lines = (
+            text.strip()
+            .replace("\t", " ")
+            .replace("\r", "")
+            .split("\n")
+        )
+        max_len = max([len(line) for line in lines])
+
+        top_line = c_tl + (c_hori * (max_len + 2)) + c_tr
+        bot_line = c_bl + (c_hori * (max_len + 2)) + c_br
+
+        return "\n".join([
+            top_line,
+            *[f"{c_vert} {line.ljust(max_len)} {c_vert}" for line in lines],
+            bot_line,
+        ])
+
+    @staticmethod
+    def print_spacer() -> None:
+        print("\n\n***\n")
+
+    @staticmethod
+    def print_title(
+            title: str,
+            title_chars: str,
+            subtitle: str|None,
+            prompt: str
+    ) -> None:
+        title = f"{Cli.print_boxed_text(title, title_chars)}\n\n"
+        subtitle = f"{subtitle}\n\n" if subtitle not in (None, "") else ""
+
+        input(
+            f"{Ansi.F_BOLD}{title}{Ansi.F_RESET}"
+            f"{Ansi.F_ITALIC}{subtitle}{Ansi.F_RESET}"
+            f"{Ansi.F_BOLD}{prompt}{Ansi.F_RESET}"
+        )
+
+    @staticmethod
+    def print_context(context: str|list[str]) -> None:
+        context = ArgParse.str_list(context)
+        print(f"{Ansi.F_ITALIC}{"\n".join(context)}{Ansi.F_RESET}")
+
+    @staticmethod
+    def print_directive(directive: str|list[str]) -> None:
+        directive = ArgParse.str_list(directive)
+        print(f"{Ansi.F_BOLD}{"\n".join(directive)}{Ansi.F_RESET}")
+
+    @staticmethod
+    def print_list(items: list[str]) -> None:
+        i_just = len(f"{len(items)}")
+        for i, item in enumerate(items):
+            i_str = f"{i+1}.".ljust(i_just+1)
+            print(f"{i_str} {item}")
+
+    @staticmethod
+    def prompt_list(items: list[str], prompt: str) -> int:
+        Cli.print_directive(f"{prompt}\n")
+        Cli.print_list(items)
+        while True:
+            user_input = input("> ")
+            try:
+                idx = int(user_input) - 1
+                if 0 <= idx < len(items):
+                    return idx
+            except ValueError:
+                pass
