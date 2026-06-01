886bf5f6090a2c5e5aaa3e451102fb8b28acf495
Ana Jahnel <48846277+anastaci1a@users.noreply.github.com>
2026-06-01 18:24:11 -0400

    consistent serialization sorting

--- src/library/data.py
+++ src/library/data.py
@@ -14,6 +14,7 @@ import inspect
 
 from library.util import ArgParse
 
+
 # export
 
 type Tags      = Mapping[str, frozenset[str]]
@@ -31,7 +32,7 @@ class TagUtil:
     @staticmethod
     def serialize(tags: Tags) -> dict[str, list[str]]:
         unfrozen: dict[str, list[str]] = {
-            k: [str(s) for s in v]
+            k: sorted([str(s) for s in v])
             for k, v in tags.items()
         }
         return unfrozen
