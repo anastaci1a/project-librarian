c6ddc61b9cb9d5d5a9259589b912f88a354f9b6d
Ana Jahnel <48846277+anastaci1a@users.noreply.github.com>
2026-06-01 22:46:05 -0400

    FUNCTIONAL library management (basic start)

---
+++ src/library/config.py
@@ -0,0 +1,32 @@
+# dep
+
+from dataclasses import dataclass
+from pathlib     import Path
+
+from library.util import SomePath
+
+
+# export
+
+@dataclass
+class LibraryConfig:
+    # attrs
+
+    config_json:      SomePath = ".library/config.json"
+    cached_json:      SomePath = ".library/cached.json"
+    folder_meta_json: SomePath = ".folder/meta.json"
+
+    # parse
+
+    def __post_init__(self):
+        for k in self.__dict__.keys():
+            path = self.__getattribute__(k)
+            self.__setattr__(k, Path(path))
+
+    # conversion
+
+    def to_dict(self):
+        r = self.__dict__.copy()
+        for k in r.keys():
+            r[k] = str(r[k])
+        return r
--- src/library/library.py
+++ src/library/library.py
@@ -1,17 +1,48 @@
 # dep
 
 from __future__  import annotations
+
+import os
+
 from dataclasses import dataclass
-from functools import cached_property
+from functools   import cached_property
+from pathlib     import Path
 
-from .data import Meta, Tags, TagUtil
+from .data   import Meta, Tags, TagUtil
+from .config import LibraryConfig
+from .util   import JSONFile, SomePath, resolve_parents
 
 
-# export
+# folders
 
 @dataclass
 class Folder:
     meta: Meta
+
+    # load
+
+    @classmethod
+    def load(
+            cls,
+            library_root: Path,
+            folder_name:  str,
+            schema:       LibraryConfig = LibraryConfig()
+    ) -> Folder:
+        path_root = library_root.joinpath(folder_name)
+        path_meta = path_root.joinpath(schema.folder_meta_json)
+        try:
+            meta_raw = JSONFile.read(path_meta)
+            meta     = Meta.from_dict(meta_raw)
+        except FileNotFoundError:
+            meta = Meta(
+                name=folder_name,
+                path_root=path_root
+            )
+            JSONFile.write(path_meta, meta.to_dict())
+        return cls(meta)
+
+
+# library
 
 class Library:
     _UNCACHE_ON_UPDATE = [
@@ -20,10 +51,41 @@ class Library:
 
     # constr
 
-    def __init__(self):
+    def __init__(
+            self,
+            library_root: SomePath,
+            config:       LibraryConfig = LibraryConfig()
+    ):
+        self._path_root: Path = Path(library_root)
         self._folders: list[Folder] = []
+        self._config: LibraryConfig = config
+        self._paths:  LibraryConfig = LibraryConfig(
+            config_json=self._path_root.joinpath(self._config.config_json),
+            cached_json=self._path_root.joinpath(self._config.config_json),
+        )
 
-    # get
+        self._init_library()
+        self.rescan()
+
+    # external (filesystem)
+
+    def _init_library(self):
+        JSONFile.write(
+            self._paths.config_json,
+            self._config.to_dict()
+        )
+
+    def rescan(self):
+        scanned = [
+            f for f in os.listdir(self._path_root)
+            if f[0] != "." # exclude sys dirs
+        ]
+        for f in scanned:
+            self._folders.append(
+                Folder.load(self._path_root, f, self._config)
+            )
+
+    # internal
 
     @property
     def folders(self) -> list[Folder]:
