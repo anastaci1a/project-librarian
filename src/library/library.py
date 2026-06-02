ac1ec79ee93942bf43db67cacc9a3e3d4c5a5dbe
Ana Jahnel <48846277+anastaci1a@users.noreply.github.com>
2026-06-02 01:32:48 -0400

    ROBUSTENED library init/load

--- src/library/library.py
+++ src/library/library.py
@@ -10,7 +10,7 @@ import os
 
 from .data   import Meta, Tags, TagUtil
 from .config import LibraryConfig
-from .util   import JSONFile, SomePath, resolve_parents
+from .util   import JSONFile, SomePath
 
 
 # folders
@@ -29,8 +29,8 @@ class Folder:
             config: LibraryConfig | None = None
     ) -> Folder:
         config = config or LibraryConfig()
-        path_root = library_root / Path(folder_name)
-        path_meta = path_root.joinpath(config.folder_meta_json)
+        path_root = library_root / folder_name
+        path_meta = path_root / config.folder_meta_json
         try:
             meta_raw = JSONFile.read(path_meta)
             meta = Meta.from_dict(meta_raw)
@@ -57,49 +57,75 @@ class Library:
     def __init__(
             self,
             library_root: SomePath,
-            config: LibraryConfig | None = None
+            config: LibraryConfig|None = None,
+
+            config_create_if_missing: bool = True,
+            config_allow_overwrite:   bool = False
     ):
-        self._path_root: Path = Path(library_root)
-        self._config = config or LibraryConfig()
-        self._paths  = self._config.resolve(self._path_root)
-
         self._folders: list[Folder] = []
+        self._assign_config_and_paths(
+            config,
+            Path(library_root)
+        )
 
-        self._init_library()
+        config_was_provided = config is not None
+        self._init_library(
+            config_was_provided,
+            config_create_if_missing,
+            config_allow_overwrite,
+        )
         self.rescan()
 
-    # external (filesystem)
+    # util
 
-    def _init_library(self) -> None:
-        should_write_config = True
+    def _assign_config_and_paths(
+            self,
+            config: LibraryConfig | None,
+            library_root: Path
+    ):
+        self._config = config or LibraryConfig()
+        self._paths = self._config.resolve(library_root)
+
+    # file init/loading
+
+    def _init_library(self, config_was_provided: bool, create_if_missing: bool, allow_overwrite: bool) -> None:
         try:
-            config_raw = JSONFile.read(self._paths.config_json)
-            config_read = LibraryConfig.from_dict(config_raw)
-            if config_read == self._config:
-                should_write_config = False # keep if meta is equal
-            else:
-                # overwrite / write new config if different
-                to_remove = config_read.resolve(self._path_root)
-                to_remove.config_json.unlink()                # delete old files
+            config_found_dict = JSONFile.read(self._paths.config_json)
+            config_found = LibraryConfig.from_dict(config_found_dict)
+            if not config_was_provided or config_found == self._config:
+                # use existing config
+                write_new_config = False
+                self._assign_config_and_paths(config_found, self._paths.root)
+            elif allow_overwrite:
+                # delete old config and overwrite
+                write_new_config = True
+                to_remove = config_found.resolve(self._paths.root)
+                to_remove.config_json.unlink() # delete old files
                 to_remove.cached_json.unlink(missing_ok=True) # ..
+            else:
+                raise FileExistsError("Found existing configuration file(s), but config_allow_overwrite is disabled.")
         except FileNotFoundError:
-            pass
-        if should_write_config:
+            write_new_config = True # new config if none found
+            if not create_if_missing:
+                raise FileNotFoundError("Did not find configuration file(s), but create_if_missing is disabled.")
+        # (write)
+        if write_new_config:
             JSONFile.write(
                 self._paths.config_json,
                 self._config.to_dict()
             )
 
     def rescan(self) -> None:
+        self._folders.clear()
         scanned = [
-            f for f in os.listdir(self._path_root)
-            if f[0] != "." # exclude sys dirs
+            d for d in os.scandir(self._paths.root)
+            if d.is_dir() and not d.name.startswith(".") # only non-sys dirs
         ]
-        for f in scanned:
+        for d in scanned:
             self._folders.append(
-                Folder.load(self._path_root, f, self._config)
+                Folder.load(self._paths.root, d.name, self._config)
             )
-        self._uncache_deps()
+        self._uncache_props()
 
     # internal
 
@@ -121,8 +147,8 @@ class Library:
 
     def add_folders(self, *folders: Folder):
         self._folders.extend(folders)
-        self._uncache_deps()
+        self._uncache_props()
 
-    def _uncache_deps(self):
+    def _uncache_props(self):
         for d in Library._UNCACHE_ON_UPDATE:
             self.__dict__.pop(d, None)
