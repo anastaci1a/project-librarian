154fb90be3d91f426ff93333d0b934e559d51be0
Ana Jahnel <48846277+anastaci1a@users.noreply.github.com>
2026-06-04 21:33:54 -0400

    more opinionated Serializable

--- src/library/_util/data/base/serializable.py
+++ src/library/_util/data/base/serializable.py
@@ -8,10 +8,30 @@ from typing import Any, Self
 from ...._util.file import JSONFile, SomePath
 
 
-# types
+# typing utils
 
 type JSONPrimitive = None | bool | int | float | str
-type JSONValue = JSONPrimitive | list[JSONPrimitive] | dict[str, JSONPrimitive]
+type JSONValue = JSONPrimitive | list[JSONValue] | dict[str, JSONValue]
+
+def is_jsonvalue(value: Any) -> bool:
+    if isinstance(value, None | bool | int | float | str):
+        return True
+
+    if isinstance(value, list):
+        return all(
+            is_jsonvalue(item) for item in value
+        )
+
+    if isinstance(value, dict):
+        return all(
+            isinstance(k, str) and is_jsonvalue(v)
+            for k, v in value.items()
+        )
+
+    return False
+
+def is_serializable(t: Any) -> bool:
+    return isinstance(t, type) and issubclass(t, Serializable)
 
 
 # data
@@ -19,27 +39,28 @@ type JSONValue = JSONPrimitive | list[JSONPrimitive] | dict[str, JSONPrimitive]
 class Serializable[T](ABC):
     # constr
 
-    def __init__(self, value: Any, **kwargs: Any) -> None:
-        self._value: T = self._parse(value, **kwargs)
+    def __init__(self, unparsed: Any, **kwargs: Any) -> None:
+        self._data = type(self)._parse(unparsed, **kwargs)
+        self._data: T = type(self)._parse(unparsed, **kwargs)
 
     # prop
 
     @property
-    def value(self) -> T:
-        return self._value # unsafe by default
+    def data(self) -> T:
+        return self._data # unsafe by default
 
     # parsing
 
     @classmethod
     @abstractmethod
-    def _parse(cls, data: Any|JSONValue, **kwargs: Any) -> T:
-        pass
+    def _parse(cls, unparsed: Any|JSONValue, **kwargs: Any) -> T:
+        raise NotImplementedError
 
     # conversions
 
     @abstractmethod
     def serialize(self, **kwargs: Any) -> JSONValue:
-        pass
+        raise NotImplementedError
 
     @classmethod
     def from_serialized(cls, serialized: JSONValue, **kwargs: Any) -> Self:
